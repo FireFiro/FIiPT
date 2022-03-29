9c0338e2ae17de079e695badd8a58bddfe67b22b
FireFiro <43435417+FireFiro@users.noreply.github.com>
2022-03-30 01:00:50 +1000

    main

--- data/prepod.js
+++ data/prepod.js
@@ -5,7 +5,6 @@ var prepod = [
         "modal": {
             "id":"testID0",
             "text": `Text Modal0`
-<<<<<<< HEAD:data/prepod.js
         }
     },
     {
@@ -14,47 +13,30 @@ var prepod = [
         "modal": {
             "id":"testID1",
             "text": `Text Modal1`
-=======
->>>>>>> d91061366acb754f81619aab6db3e190f5e178ed:js/prepod.js
-        }
-    },
-    {
-        "fio":"test1",
-        "image": "https://cdn.discordapp.com/attachments/712187625720774687/957965555812433971/pexels-mikael-blomkvist-6476587.jpg",
-        "modal": {
-<<<<<<< HEAD:data/prepod.js
-            "id":"testID2",
-            "text": `Text Modal2`
-=======
-            "id":"testID1",
-            "text": `Text Modal1`
->>>>>>> d91061366acb754f81619aab6db3e190f5e178ed:js/prepod.js
         }
     },
     {
         "fio":"test2",
         "image": "https://cdn.discordapp.com/attachments/712187625720774687/957965555812433971/pexels-mikael-blomkvist-6476587.jpg",
         "modal": {
-<<<<<<< HEAD:data/prepod.js
-            "id":"testID3",
-            "text": `Text Modal3`
-=======
             "id":"testID2",
             "text": `Text Modal2`
->>>>>>> d91061366acb754f81619aab6db3e190f5e178ed:js/prepod.js
         }
     },
     {
         "fio":"test3",
         "image": "https://cdn.discordapp.com/attachments/712187625720774687/957965555812433971/pexels-mikael-blomkvist-6476587.jpg",
         "modal": {
-<<<<<<< HEAD:data/prepod.js
-            "id":"testID4",
-            "text": `Text Modal4`
-=======
             "id":"testID3",
             "text": `Text Modal3`
->>>>>>> d91061366acb754f81619aab6db3e190f5e178ed:js/prepod.js
+        }
+    },
+    {
+        "fio":"test4",
+        "image": "https://cdn.discordapp.com/attachments/712187625720774687/957965555812433971/pexels-mikael-blomkvist-6476587.jpg",
+        "modal": {
+            "id":"testID4",
+            "text": `Text Modal4`
         }
     },
     {
@@ -69,13 +51,8 @@ var prepod = [
         "fio":"test6",
         "image": "https://cdn.discordapp.com/attachments/712187625720774687/957965555812433971/pexels-mikael-blomkvist-6476587.jpg",
         "modal": {
-<<<<<<< HEAD:data/prepod.js
             "id":"testID6",
             "text": `Text Modal6`
-=======
-            "id":"testID4",
-            "text": `Text Modal4`
->>>>>>> d91061366acb754f81619aab6db3e190f5e178ed:js/prepod.js
         }
     }
 ]
