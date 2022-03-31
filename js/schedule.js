31371258a7a33710e8bcd131ca2eca3b7efd7c84
FireFiro <43435417+FireFiro@users.noreply.github.com>
2022-03-31 23:07:15 +1000

    Merge branch 'main' of https://github.com/FireFiro/FIiPT

--- js/schedule.js
+++ js/schedule.js
@@ -15,14 +15,14 @@ $(document).ready(function() {
         result = `<table class="table table-hover" week="${schedules.week}" group="${schedules.group}">
         <thead>
           <tr>
-            <td colspan="2">${schedules.group}</td>
-            <td colspan="2">${schedules.week}</td>
+            <td colspan="2" style="color:white">${schedules.group}</td>
+            <td colspan="2" style="color:white">${schedules.week}</td>
           </tr>
           <tr>
-            <th scope="col">№</th>
-            <th scope="col">Название</th>
-            <th scope="col">Преподователь</th>
-            <th scope="col">Кабинет</th>
+            <th scope="col" style="color:white">№</th>
+            <th scope="col" style="color:white">Название</th>
+            <th scope="col" style="color:white">Преподователь</th>
+            <th scope="col" style="color:white">Кабинет</th>
           </tr>
         </thead>
         <tbody>
@@ -35,10 +35,10 @@ $(document).ready(function() {
         
         curentItem++
         predmet += `<tr>
-        <th scope="row">${curentItem}</th>
-        <td>${lessons.name}</td>
-        <td>${lessons.teacher}</td>
-        <td>${lessons.office}</td>
+        <th scope="row" style="color:white">${curentItem}</th>
+        <td style="color:white">${lessons.name}</td>
+        <td style="color:white">${lessons.teacher}</td>
+        <td style="color:white">${lessons.office}</td>
       </tr>`
     }
 
